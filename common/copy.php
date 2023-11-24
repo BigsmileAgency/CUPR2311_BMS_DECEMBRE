19f0cce51c5d4fa29c07d566e0431ae291447e40
dylan <dylan @bigsmile.be>
2023-11-24 10:35:01 +0100

    Nbsp on FR

--- common/copy.php
+++ common/copy.php
@@ -2,7 +2,5 @@
   /**
    * Exemples: usefull->copy.php
    */
-  $copies["copy"]["FR"][] = "Défier ses habitudes";
-
-
+  $copies["copy"]["FR"][] = "Défier ses&nbsp;habitudes";
   $copies["copy"]["NL"][] = "Doorbreek uw&nbsp;gewoontes";
